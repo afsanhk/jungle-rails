3ff821989568b53ca71564dbcd32352f11451154
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-23 16:42:08 +0000

    Add more tests for user authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates :name, presence: true
+  validates :email, uniqueness: true, case_sensitive: false, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,23 +4,36 @@ RSpec.describe User, type: :model do
   describe "Validations" do 
 
     it 'is valid with matching password and password_confirmation fields' do
-    @user = User.new
-    puts @user.inspect
+      @user = User.new(name: "Chicken", email: "1@1.com", password:"pollo1", password_confirmation:"pollo1")
+      expect(@user).to be_valid
     end
 
     it 'is invalid with non-matching password and password_confirmation fields' do
+      @user = User.new(name: "Chicken", email: "1@1.com", password:"poulet", password_confirmation:"pollo")
+      expect(@user).to_not be_valid
     end
 
     it 'is valid with unique emails' do 
+      @user1 = User.new(name: "Chicken", email: "1@1.com", password:"pollo1", password_confirmation:"pollo1")
+      @user2 = User.new(name: "Chicken", email: "2@1.com", password:"pollo1", password_confirmation:"pollo1")
+      expect(@user1).to be_valid
+      expect(@user2).to be_valid
     end
 
     it 'is invalid with repeated emails' do 
+      @user1 = User.new(name: "Chicken", email: "1@1.com", password:"pollo1", password_confirmation:"pollo1")
+      expect(@user1).to be_valid
+      @user1.save!
+      @user2 = User.new(name: "Chicken", email: "1@1.com", password:"pollo1", password_confirmation:"pollo1")
+      expect(@user2).to_not be_valid
     end
 
     it 'requires emails, first name and last name' do
     end
 
     it 'must have a minimum password length when a user account is being created' do
+      @user = User.new(name: "Chicken", email: "1@1.com", password:"four", password_confirmation:"pollo")
+      expect(@user).to_not be_valid
     end
 
     it 'must ignore leading and trailing spaces for e-mails' do
