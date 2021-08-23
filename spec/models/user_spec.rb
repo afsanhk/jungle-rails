02c041bbfe34545f40b73a654e398380afa34316
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-23 16:10:48 +0000

    Add test blocks. Testing logic not added yet.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do 
+
+    it 'is valid with matching password and password_confirmation fields' do
+    end
+
+    it 'is invalid with non-matching password and password_confirmation fields' do
+    end
+
+    it 'is valid with unique emails' do 
+    end
+
+    it 'is invalid with repeated emails' do 
+    end
+
+    it 'requires emails, first name and last name' do
+    end
+
+    it 'must have a minimum password length when a user account is being created' do
+    end
+
+    it 'must ignore leading and trailing spaces for e-mails' do
+    end
+
+    it 'must be case insensitive for e-mails' do
+    end
+
+  end
 end
