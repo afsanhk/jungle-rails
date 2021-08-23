ccf13a1d6a292ff325afc254c116a303fd52f005
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-23 19:15:57 +0000

    Add base code to test product_details featuure using capybara.

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product detail page by clicking on a product", type: :feature, js: true do
+  
+  #SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  #ACT
+
+  #DEBUG
+  #save_screenshot
+
+  #VERIFY
+end
